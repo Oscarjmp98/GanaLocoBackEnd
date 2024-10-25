25c7030651f8223e0257e25c1f9e2b97dc158b91
Oscar Medina Paz <oscar.medina01@unicatolica.edu.co>
2024-10-24 23:12:07 -0500

    Configuracion  mongo db

--- api-json/index.js
+++ api-json/index.js
@@ -18,8 +18,11 @@ app.use(cors({
 app.use(express.json());
 app.use('/v1/routes', router);
 
-mongoose.connect(process.env.MONGODB_URI, {
-  })
+/*mongoose.connect(process.env.MONGODB_URI, {
+  })*/
+mongoose.connect('mongodb+srv://OscarJM:BdReYjum8s047XyT@ganaloco.a1u9b.mongodb.net/GanaLoco?retryWrites=true&w=majority&appName=GanaLoco',{})
+
+
 .then(() => console.log('Conectado a MongoDB Atlas'))
 .catch(err => console.error('Error de conexión a MongoDB:', err));
 
