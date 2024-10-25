85c844dcd86adb027c44f5c1a87b87fdf089fbbd
Oscar Medina Paz <oscar.medina01@unicatolica.edu.co>
2024-10-24 23:45:04 -0500

    correcion de vista  de fecha en tabla

--- api-json/routes/controllers/Controller.js
+++ api-json/routes/controllers/Controller.js
@@ -67,7 +67,7 @@ const createUser = async (req, res) => {
 /*---------------------------------------------Registar Codigos-------------------------------------------- */
 const codeSchema = new mongoose.Schema({
   code: {type:String},
-  date: { type: Date, default: Date.now },
+  fecha: { type: Date, default: Date.now },
   Ganador: {type: Boolean}
 });
 const Code = mongoose.model('Code', codeSchema, "codigos");
